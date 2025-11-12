8625750fdd948034509fcb6d6100828adac12aeb
Alexey Kavderko <kavderko98@gmail.com>
2025-11-12 16:25:51 +0200

    fix

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,26 +1,7 @@
-import { NoteTag } from "@/types/note";
-import css from "./SidebarNotes.module.css";
-import Link from "next/link";
+import SidebarNotes from '@/components/SidebarNotes/SidebarNotes';
 
-const TagsMenu = () => {
-  const noteTags = Object.values(NoteTag);
-
-  return (
-    <ul className={css.menuList}>
-      <li className={css.menuItem}>
-        <Link className={css.menuLink} href={"/notes/filter/All"}>
-          All notes
-        </Link>
-      </li>
-      {noteTags.map((tag) => (
-        <li key={tag} className={css.menuItem}>
-          <Link className={css.menuLink} href={`/notes/filter/${tag}`}>
-            {tag}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
+const NotesSidebar = async () => {
+  return <SidebarNotes />;
 };
 
-export default TagsMenu;
+export default NotesSidebar;
